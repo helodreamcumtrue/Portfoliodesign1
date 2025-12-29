a17b2a58178cd27a9cad734ce974acfcadca7afe
helodreamcumtrue <dreamycrafts56@gmail.com>
2025-12-29 15:31:46 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+5. Place all the configuration files I just generated(`vite.config.js`, `tailwind.config.js`, etc.) and the `package.json` into the main `lakshay-portfolio` folder.
